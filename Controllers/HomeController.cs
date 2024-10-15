df19fd3b85610aebbea7e3a763ac9ec8e0373d41
Daniella <dani2610@live.dk>
2024-10-15 16:29:45 +0200

    Edit homecontroller to do everything.... Added dropdown

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,23 +1,101 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using ToDoApp.Models;
 
-namespace ToDoApp.Controllers
+namespace ToDoApp.Controllers //Del klassen op, så det her er startsiden og den implenter et compoment der er toDOListen, frem for at alt sker på denne
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private ToDoContext context;
+        public HomeController(ToDoContext ctx) => context = ctx;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public IActionResult Index(string id)
         {
-            _logger = logger;
+            var filters = new Filters(id);
+            ViewBag.Filters = filters;
+            ViewBag.Categories = context.Categories.ToList();
+            ViewBag.Statuses = context.StatusSet.ToList();
+            ViewBag.DueFilterValues = Filters.DueFilterValues;
+            IQueryable<ToDo> query = context.ToDoSet
+                .Include(t => t.Category)
+                .Include(t => t.Status);
+
+            if (filters.HasCategory)
+            { query = query.Where(t => t.CategoryId == filters.CategoryId); }
+            if (filters.HasStatus)
+            { query = query.Where(t => t.StatusId == filters.StatusId); }
+            if (filters.HasDueAt)
+            {
+                var today = DateTime.Today;
+                if (filters.IsPast)
+                { query = query.Where(t => t.DueDate < today); }
+                else if (filters.IsFuture)
+                { query = query.Where(t => t.DueDate > today); }
+                else if (filters.IsToday)
+                { query = query.Where(t => t.DueDate == today); }
+            }
+            var tasks = query.OrderBy(t => t.DueDate).ToList();
+            return View(tasks);
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Add()
         {
-            return View();
+            ViewBag.Categories = context.Categories.ToList();
+            ViewBag.Statuses = context.StatusSet.ToList();
+            var task = new ToDo { StatusId = "open" };
+            return View(task);
+        }
+        [HttpPost]
+        public IActionResult Add(ToDo task)
+        {
+            if (ModelState.IsValid)
+            {
+                context.ToDoSet.Add(task);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Categories = context.Categories.ToList();
+                ViewBag.Statuses = context.StatusSet.ToList();
+                return View(task);
+            }
+        }
+        [HttpPost]
+        public IActionResult Filter(string[] filter)
+        {
+            var id = string.Join("-", filter);
+            return RedirectToAction("Index", new { ID = id });
         }
 
+        [HttpPost]
+        public IActionResult MarkComplete([FromRoute]string id, ToDo selected)
+        {
+            selected = context.ToDoSet.Find(selected.Id);
+            if (selected != null)
+            {
+                { selected.StatusId = "closed"; }
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index", new { ID = id });
+        }
+        [HttpPost]
+        public  IActionResult DeleteComplete(string id)
+        {
+            var toDelete = context.ToDoSet.Where(t => t.StatusId == "closed");
+            foreach (var task in toDelete)
+            {
+                context.ToDoSet.Remove(task);
+            }
+            context.SaveChanges();
+            return RedirectToAction("Index", new { ID = id });
+        }
+
+
+        /*
         public IActionResult Privacy()
         {
             return View();
@@ -27,6 +105,6 @@ namespace ToDoApp.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        }*/
     }
 }
---
+++ Models/Filters.cs
@@ -0,0 +1,35 @@
+﻿namespace ToDoApp.Models
+{
+    public class Filters
+    {
+        public Filters(string filtersting)
+        {
+            FilterString = filtersting ?? "all-all-all";
+            string[] filterParts = FilterString.Split('-');
+            CategoryId = filterParts[0];
+            DueAt = filterParts[1];
+            StatusId = filterParts[2];
+
+        }
+        public string FilterString { get; }
+        public string CategoryId { get; }
+        public string DueAt { get; }
+        public string StatusId { get; }
+        public bool HasCategory => CategoryId.ToLower() != "all";
+        public bool HasDueAt => DueAt.ToLower() != "all";
+        public bool HasStatus => StatusId.ToLower() != "all";
+        public static Dictionary<string, string> DueFilterValues =>
+            new Dictionary<string, string>
+            {
+                {"past", "Overdue"},
+                {"today", "Due Today"},
+                {"future", "Due in Future"}
+            };
+
+        public bool IsPast => DueAt.ToLower() == "past";
+        public bool IsFuture => DueAt.ToLower() == "future";
+        public bool IsToday => DueAt.ToLower() == "today";
+
+
+    }
+}
